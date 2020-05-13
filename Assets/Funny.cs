e504015f8006512e9f6916b6211b37f5adce2281
Pip pip <piptoepatana@gmail.com>
2020-05-13 00:33:36 -0600

    All for today.

---
+++ Assets/Funny.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Funny : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = (Rigidbody)GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        rb.AddForce(0, 0.1f, 0.1f, ForceMode.Acceleration);
+    }
+}
